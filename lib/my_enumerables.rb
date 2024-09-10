b821785715f4ce8a3c1af246fe94eaf3facf687e
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-09-10 13:44:17 -0400

    Define my_each in Array class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,16 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    # Return an enumerator if no block is given
+    return to_enum(:my_each) unless block_given?
+
+    # Iterate over each element and yield to the block
+    for element in self
+      yield(element)
+    end
+
+    # Return the original array
+    self
+  end
 end
